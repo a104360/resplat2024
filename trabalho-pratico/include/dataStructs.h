6c3f59c4ecba7948891eb48ddd9243f340f4aaef
a104360 <a104360@alunos.uminho.pt>
2023-10-25 15:34:24 +0100

    Hash Tables inicio

--- trabalho-pratico/include/dataStructs.h
+++ trabalho-pratico/include/dataStructs.h
@@ -1,4 +1,17 @@
 #ifndef DATASTRUCTS_H
 #define DATASTRUCTS_H
+#include <stdbool.h>
+
+typedef struct node Node;
+
+typedef struct htable HashTable;
+
+static HashTable * createTable();
+
+static void destroyTable(HashTable *);
+
+static int hash(int,void *);
+
+static void insertHTable(HashTable *,void *,void *);
 
 #endif
