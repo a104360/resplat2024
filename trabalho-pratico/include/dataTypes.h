1f450b7c2f8ac307c8bc0175c531eb3b16c85927
a104360 <a104360@alunos.uminho.pt>
2023-11-08 00:16:54 +0000

    initUsers/Flight/... e setters

--- trabalho-pratico/include/dataTypes.h
+++ trabalho-pratico/include/dataTypes.h
@@ -1,6 +1,8 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
+enum structs;
+
 typedef struct time Time;
 
 typedef struct user User;
@@ -10,5 +12,38 @@ typedef struct flight Flight;
 typedef struct reservation Reservation;
 
 typedef struct passanger Passanger;
+
+
+
+
+void initUser(User *);
+
+void initFlight(Flight *);
+
+void initReservation(Reservation *);
+
+void initPassanger(Passanger *);
+
+User * createUser();
+
+Flight * createFlight();
+
+Reservation * createReservation();
+
+Passanger * createPassanger();
+
+
+
+void setUserId(User *,char *);
+
+void setUserName(User *,char *);
+
+void setUserEmail(User *,char *);
+
+void setUserBday(User *,Time *);
+
+void setUserSex(User *,char *);
+
+void setUser(User *,char *);
 
 #endif
