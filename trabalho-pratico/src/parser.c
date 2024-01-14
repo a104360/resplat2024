e168c11e662e7e4492a1b9f04002ce1a24f20dc8
a104360 <a104360@alunos.uminho.pt>
2024-01-14 19:54:27 +0000

    funçoes de validacao dos csvs para as bases de dados criadas e separadas

--- trabalho-pratico/src/parser.c
+++ trabalho-pratico/src/parser.c
@@ -12,7 +12,7 @@
 #include <glib.h>
 
 
-
+#define BUFFERSIZE 1000
 
 #define CHECKLEN(line) \
     if(strlen(line) < 1 || line == NULL) return NULL;\
@@ -945,7 +945,7 @@ User * userCheck(const char * line){
 
 
 //Recieves a passenger and checks if the passenger is valid using the previus functions
- Passenger * passengerCheck(const char * line,Users * uDatabase,Flights * fDatabase){
+Passenger * passengerCheck(const char * line,Users * uDatabase,Flights * fDatabase){
     if(line[0] == ';' || line == NULL || line[0] == '\0' || line[0] == '\n') return NULL;
     char * aux = strdup(line);
     char * token = NULL;
@@ -974,4 +974,340 @@ User * userCheck(const char * line){
 
     ffree((void **) &aux);
     return passenger;
+}
+
+Users * validateUsers(const char * folderPath){
+    size_t argSize = 0;
+    argSize = strlen(folderPath);
+    char * filePath = NULL;
+    filePath = malloc(argSize + 20); // malloc
+    memset(filePath,'\0',argSize + 20);
+    strncpy(filePath,folderPath,argSize);
+
+
+    // Create Users Database
+    strncat(filePath,"/users.csv",12); // ?
+    filePath[argSize + 1 + 10 + 1] = '\0';
+
+    FILE * userFile = NULL;
+    userFile = fopen(filePath,"r"); // open
+
+    FILE * userErrors = NULL;
+    char * filePathUErrors = NULL;
+    filePathUErrors = malloc(strlen(folderPath) + 20); // malloc
+    strncpy(filePathUErrors,"Resultados",14);
+    strncat(filePathUErrors,"/users_errors.csv",18); // ?
+    userErrors = fopen(filePathUErrors,"a"); // open
+    if(userErrors == NULL) {perror("Users errors file did not open\n"); return NULL;}
+
+    char * userData = malloc(sizeof(char) * BUFFERSIZE); // malloc
+
+    if(userData == NULL) { perror("Erro a alocar memoria na main"); return NULL;}
+
+    memset(userData, '\0', BUFFERSIZE);
+
+
+    Users * uDatabase = createDatabase(&destroyUser); // initDatabase
+
+    fgets(userData,BUFFERSIZE,userFile);
+
+    fprintf(userErrors,"%s",userData);
+
+    while(fgets(userData,BUFFERSIZE,userFile)){
+
+        User * uBuffer = NULL;
+
+        int tamanhoUserData = strlen(userData);
+
+        char * userDataClean = malloc(sizeof(char) * (tamanhoUserData +1)); // malloc
+
+        strncpy(userDataClean,userData,tamanhoUserData);
+
+        userDataClean[tamanhoUserData] = '\0';
+
+
+        uBuffer = userCheck(userDataClean); // malloc
+
+
+        if(uBuffer != NULL){
+            insertOnDatabase(uDatabase,getUserId(uBuffer),uBuffer);
+
+        }else{ 
+            fprintf(userErrors,"%s",userDataClean);
+        }
+        
+        free(userDataClean);// free
+        userDataClean = NULL;
+    }
+    
+    fclose(userFile); // close
+    userFile = NULL;
+    
+    free(filePathUErrors);  // free
+    filePathUErrors = NULL;
+    
+    free(userData); // free
+    userData = NULL;
+
+    fclose(userErrors); // close
+    userErrors = NULL;
+
+    ffree((void **) &filePath);
+    return uDatabase;
+}
+
+Reservations * validateReservations(Users * uDatabase, const char * folderPath){
+    size_t argSize = 0;
+    argSize = strlen(folderPath);
+    char * filePath = NULL;
+    filePath = malloc(argSize + 20);
+    memset(filePath,'\0',argSize + 20);
+    strncpy(filePath,folderPath,argSize);
+
+    memset(filePath,'\0',argSize + 20);
+    
+        // Create Reservations Database
+        strncpy(filePath,folderPath,argSize);
+        strncat(filePath,"/reservations.csv",19);
+
+        filePath[argSize + 1 + 17 + 1] = '\0';
+
+        FILE * reservationsFile = NULL;
+        reservationsFile = fopen(filePath,"r");
+
+        FILE * reservationsErrors = NULL;
+        char * filePathRErrors = NULL;
+        filePathRErrors = malloc(argSize + 27);
+        strncpy(filePathRErrors,"Resultados",14);
+        strncat(filePathRErrors,"/reservations_errors.csv",25);
+        reservationsErrors = fopen(filePathRErrors,"a+");
+        if(!reservationsErrors) {perror("Reservations errors file did not open\n"); return NULL;}
+
+        char * reservationData = malloc(sizeof(char) * BUFFERSIZE);
+
+        if(!reservationData) { perror("Erro a alocar memoria na main"); return NULL;}
+
+        memset(reservationData, '\0', BUFFERSIZE);
+
+        Reservations * rDatabase = createDatabase(&destroyReservation);
+
+        fgets(reservationData,BUFFERSIZE,reservationsFile);
+
+        fprintf(reservationsErrors,"%s",reservationData);
+
+        while(fgets(reservationData,BUFFERSIZE,reservationsFile)){
+
+            Reservation * rBuffer = NULL;
+
+            int tamanhoReservationData = strlen(reservationData);
+
+            char * reservationDataClean = malloc(sizeof(char) * (tamanhoReservationData +1));
+
+            strncpy(reservationDataClean,reservationData,tamanhoReservationData);
+
+            reservationDataClean[tamanhoReservationData] = '\0';
+
+            rBuffer = reservationCheck(reservationDataClean,uDatabase);
+
+        if(rBuffer != NULL){
+                insertOnDatabase(rDatabase,getReservId(rBuffer),rBuffer);
+            }else{ 
+                fprintf(reservationsErrors,"%s",reservationDataClean);
+            }
+                
+                free(reservationDataClean);
+                reservationDataClean = NULL;
+        }
+        
+        fclose(reservationsFile); // close
+        reservationsFile = NULL;
+        free(filePathRErrors);  // free
+        filePathRErrors = NULL;
+        free(reservationData); // free
+        reservationData = NULL;
+        fclose(reservationsErrors); // close
+        reservationsErrors = NULL;
+
+    ffree((void **) &filePath);
+    return rDatabase;
+}
+
+Flights * validateFlights(const char * folderPath){
+    size_t argSize = 0;
+    argSize = strlen(folderPath);
+    char * filePath = NULL;
+    filePath = malloc(argSize + 20);
+    memset(filePath,'\0',argSize + 20);
+    strncpy(filePath,folderPath,argSize);
+
+    memset(filePath,'\0',argSize + 20);
+    
+
+        //Create Flights Database
+        strncpy(filePath,folderPath,argSize);
+        strncat(filePath,"/flights.csv",14);
+
+        FILE * flightFile = NULL;
+        flightFile = fopen(filePath,"r");
+
+        FILE * flightsErrors = NULL;
+        char * filePathFErrors = NULL;
+        filePathFErrors = malloc(argSize + 27);
+        strncpy(filePathFErrors,"Resultados",14);
+        strncat(filePathFErrors,"/flights_errors.csv",25);
+        flightsErrors = fopen(filePathFErrors,"a+");
+        if(!flightsErrors) {
+            perror("Flights errors file did not open\n");
+            if(filePath){
+                free(filePath);
+                filePath = NULL;
+            } 
+            if(filePathFErrors){
+                free(filePathFErrors);
+                filePathFErrors = NULL;
+            }
+            if(flightFile){
+                fclose(flightFile);
+                flightFile = NULL;
+            }
+            return NULL;
+        }
+
+        char * flightData = malloc(sizeof(char) * BUFFERSIZE);
+
+        if(!flightData) { 
+            perror("Erro a alocar memoria na main"); 
+            if(filePath){
+                free(filePath);
+                filePath = NULL;
+            } 
+            if(filePathFErrors){
+                free(filePathFErrors);
+                filePathFErrors = NULL;
+            }
+            if(flightFile){
+                fclose(flightFile);
+                flightFile = NULL;
+            }
+            fclose(flightsErrors);
+            flightsErrors = NULL;
+            return NULL;
+        }
+
+        memset(flightData, '\0', BUFFERSIZE);
+
+        Flights * fDatabase = createDatabase(&destroyFlight);
+
+        while(fgets(flightData,BUFFERSIZE,flightFile)){
+
+            Flight * fBuffer = NULL;
+
+            int tamanhoFlightData = strlen(flightData);
+
+            char flightDataClean[tamanhoFlightData +1];
+
+            strncpy(flightDataClean,flightData,tamanhoFlightData);
+
+            flightDataClean[tamanhoFlightData] = '\0';
+
+            fBuffer = fligthCheck(flightDataClean);
+
+            //fligthCheck(flightDataClean);
+
+            if(fBuffer != NULL){
+
+                insertOnDatabase(fDatabase,getFlightId(fBuffer),fBuffer);
+
+            }else{ 
+                fprintf(flightsErrors,"%s",flightDataClean);
+            }
+
+        }
+
+        fclose(flightFile); // close
+        flightFile = NULL;
+        free(filePathFErrors);  // free
+        filePathFErrors = NULL;
+        free(flightData); // free
+        flightData = NULL;
+        fclose(flightsErrors); // close
+        flightsErrors = NULL;
+
+    ffree((void **) &filePath);
+    return fDatabase;
+}
+
+Passengers * validatePassengers(Users * uDatabase,Flights * fDatabase,const char * folderPath){
+    size_t argSize = 0;
+    argSize = strlen(folderPath);
+    char * filePath = NULL;
+    filePath = malloc(argSize + 20);
+    memset(filePath,'\0',argSize + 20);
+    strncpy(filePath,folderPath,argSize);
+
+    memset(filePath,'\0',argSize + 20);
+
+        strncpy(filePath,folderPath,argSize);
+        strncat(filePath,"/passengers.csv",17);
+
+        FILE * passengersFile = NULL;
+        passengersFile = fopen(filePath,"r");
+        if(passengersFile == NULL){
+            perror("Did not opened the file correctly");
+            return NULL;
+        }
+
+        FILE * passengersErrors = NULL;
+        char * filePathPErrors = NULL;
+        filePathPErrors = malloc(argSize + 27);
+        strncpy(filePathPErrors,"Resultados",14);
+        strncat(filePathPErrors,"/passengers_errors.csv",25);
+        passengersErrors = fopen(filePathPErrors,"a+");
+        if(!passengersErrors) {perror("Passengers errors file did not open\n"); return NULL;}
+
+        char * passengersData = malloc(sizeof(char) * BUFFERSIZE);
+
+        if(!passengersData) { perror("Erro a alocar memoria na main"); return NULL;}
+
+        memset(passengersData, '\0', BUFFERSIZE);
+
+        Passengers * pDatabase = createPassengerDatabase();
+        
+        fgets(passengersData,BUFFERSIZE,passengersFile);
+
+        fprintf(passengersErrors,"%s",passengersData);
+
+        while(fgets(passengersData,BUFFERSIZE,passengersFile)){
+
+            Passenger * pBuffer = NULL;
+
+            int tamanhoPassengersData = strlen(passengersData);
+
+            char passengersDataClean[tamanhoPassengersData +1];
+
+            strncpy(passengersDataClean,passengersData,tamanhoPassengersData);
+
+            passengersDataClean[tamanhoPassengersData] = '\0';
+
+            pBuffer = passengerCheck(passengersDataClean,uDatabase,fDatabase);
+
+            if(pBuffer != NULL){
+                insertPassenger(pDatabase,pBuffer);
+
+            }else{ 
+                fprintf(passengersErrors,"%s",passengersDataClean);
+            }
+        }
+
+        fclose(passengersFile); // close
+        passengersFile = NULL;
+        free(filePathPErrors);  // free
+        filePathPErrors = NULL;
+        free(passengersData); // free
+        passengersData = NULL;
+        fclose(passengersErrors); // close
+        passengersErrors = NULL;
+
+    ffree((void **) &filePath);
+    return pDatabase;
 }
